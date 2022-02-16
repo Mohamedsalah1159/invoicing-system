eedf613c4198e4574547b4b67abfca11bee6a68f
mohamed_salah7901 <mohamed.salah7901@gmail.com>
2022-02-16 14:14:24 -0800

    v1

---
+++ app/Http/Controllers/SectionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Section;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('sections.section');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $exists = Section::where('section_name', "=", $input['section_name'])->exists();
+        if($exists){
+            session()->flash('Error', 'This Section is Exists');
+            return redirect('bill/sections');
+        }else{
+            Section::create([
+                'section_name' => $request->section_name,
+                'descreption' => $request->descreption,
+                'created_by' => (Auth::user()->name)
+
+            ]);
+            session()->flash('Add', 'This Section is added succesfully');
+            return redirect('bill/sections');
+        }
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Section $section)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Section $section)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Section $section)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Section $section)
+    {
+        //
+    }
+}
